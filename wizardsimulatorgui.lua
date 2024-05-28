7cd05ea18a1eb3a2854b8cce8402fb7c2c607839
penguin586970 <136866320+P3nguinMinecraft@users.noreply.github.com>
2024-05-27 22:29:59 -0400

    Add files via upload

---
+++ wizardsimulatorgui.lua
@@ -0,0 +1,274 @@
+print("Loading Wizard Simulator GUI")
+
+
+getgenv().SecureMode = true
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+-- local vars
+local Player = game:GetService("Players").LocalPlayer
+local Humanoid = Player.Character.Humanoid
+local Workspace = game:GetService("Workspace")
+local UserInputService = game:GetService("UserInputService")
+local SelectedQuest = {"Default"}
+local AutoQuestToggle = false
+local WalkspeedToggleOld = false
+local WalkspeedToggle = false
+local Walkspeed = 16
+local JumpPowerToggle = false
+local JumpPower = 50
+local toggle = false
+local HealthPercentage = Humanoid.Health / Humanoid.MaxHealth * 100
+
+
+
+spawn(function()
+   while wait(0.1) do
+      if WalkspeedToggleOld == true and WalkspeedToggle == false then
+         Humanoid.WalkSpeed = 16
+      end
+      if WalkspeedToggle then
+         Humanoid.WalkSpeed = Walkspeed
+      end
+      if JumpPowerToggleOld == true and JumpPowerToggle ==false then
+         Humanoid.JumpPower = 50
+      end
+      if JumpPowerToggle then
+         Humanoid.JumpPower = JumpPower
+      end
+      WalkspeedToggleOld = WalkspeedToggle
+      JumpPowerToggleOld = JumpPowerToggle
+   end
+end)
+
+
+local Window = Rayfield:CreateWindow({
+   Name = "Wizard Simulator",
+   LoadingTitle = "Wizard Simulator GUI",
+   LoadingSubtitle = "by penguin",
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = "wizardsimulatorgui", -- Create a custom folder for your hub/game
+      FileName = "wizardsimulator_config"
+   },
+   Discord = { 
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = false -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key wiCH be saved, but if you change the key, they wiCH be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"HeCHo"} -- List of keys that wiCH be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("heCHo","key22")
+   }
+})
+
+local CreditTab = Window:CreateTab("Info", nil) -- Title, Image
+
+local CreditLabel1 = CreditTab:CreateLabel("Developed by penguin586970")
+
+local CreditLabel2 = CreditTab:CreateLabel("Executor used: MacSploit (not affiliated!)")
+
+local CreditLabel3 = CreditTab:CreateLabel("For questions, concerns, contact windows1267 on discord")
+
+
+
+local QuestTab = Window:CreateTab("Quest", nil) -- Title, Image
+
+local QuestParagraph1 = QuestTab:CreateParagraph({Title = "Quest Manipulation", Content = "Give yourself quests with this function, no matter the distance and level you are! DO NOT SPAM THESE REMOTES! NOTE THAT THERE IS A SMALL COOLDOWN BETWEEN FINISHING AND RECEIVING A QUEST."})
+
+local QuestDropdown1 = QuestTab:CreateDropdown({
+   Name = "Select Quest",
+   Options = {
+      "LJ:1","LJ:2","LJ:3","LJ:4","LJ:5","LJ:6","LJ:7","LJ:8",
+      "BT:1","BT:2","BT:3","BT:4","BT:5","BT:6","BT:7","BT:8","BT:9",
+      "CH:1","CH:2","CH:3","CH:4","CH:5","CH:6","CH:7","CH:8",
+      "LP:1","LP:2","LP:3","LP:4","LP:5","LP:6","LP:7","LP:8","LP:9","LP:10","LP:11","LP:12","LP:13",
+      "WS:1","WS:2","WS:3","WS:4","WS:5","WS:6","WS:7","WS:8","WS:9","WS:10","WS:11",
+      "OM:1","OM:2","OM:3","OM:4","OM:5","OM:6","OM:7","OM:8","OM:9","OM:10","OM:11",
+      "VB:1","VB:2","VB:3","VB:4","VB:5","VB:6","VB:7","VB:8","VB:9","VB:10",
+      "VD:1","VD:2","VD:3","VD:4","VD:5","VD:6","VD:7",
+      "CJ:1","CJ:2","CJ:3","CJ:4","CJ:5","CJ:6","CJ:7","CJ:8","CJ:9","CJ:10","CJ:11"
+   },
+   CurrentOption = {"LJ:1"},
+   MultipleOptions = false,
+   Flag = "QuestDropdown1",
+   Callback = function(Option)
+      SelectedQuest = Option
+   end,
+})
+
+local QuestButton1 = QuestTab:CreateButton({
+   Name = "Give Quest",
+   Callback = function()
+if SelectedQuest[1] then
+         game:GetService("ReplicatedStorage").Remote.AcceptQuest:FireServer(SelectedQuest[1])
+      else
+         Rayfield:Notify({
+            Title = "Error",
+            Content = "No quest selected! How did this happen?",
+            Duration = 5,
+            Image = nil,
+            Actions = { -- Notification Buttons
+               Ignore = {
+                  Name = "OK",
+                  Callback = function()
+               end
+               },
+            },
+         })
+      end
+   end,
+})
+
+local QuestButton2 = QuestTab:CreateButton({
+   Name = "Cancel Quest",
+   Callback = function()
+      game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("CancelQuest"):FireServer()
+   end,
+})
+
+local QuestToggle1 = QuestTab:CreateToggle({
+   Name = "Auto Give CJ:4 Quest",
+   CurrentValue = false,
+   Flag = "QuestToggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+      AutoQuestToggle = Value
+   end,
+})
+
+local QuestParagraph2 = QuestTab:CreateParagraph({Title = "Auto Give", Content = "CJ:4 is Kill 1 Training Dummy (in Training World) but gives a ton of XP and Gold, useful for super fast progression in all game stages. Auto Give CJ:4 Quest function automatically gives you the quest whenever you press E or Q (assuming you one shot Dummies)."})
+
+
+
+local PotionTab = Window:CreateTab("Potion", nil) -- Title, Image
+
+local PotionSection1 = PotionTab:CreateSection("Health")
+
+local PotionButton1 = PotionTab:CreateButton({
+   Name = "Get Health Potion",
+   Callback = function()
+      firetouchinterest(Player.HumanoidRootPart, Workspace.Effects.HealthPotion.HealthPotion.Forcefield.Touchinterest, 0)
+   end,
+})
+
+
+
+local PotionSection2 = PotionTab:CreateSection("Mana")
+
+local PotionButton2 = PotionTab:CreateButton({
+   Name = "Get Mana Potion",
+   Callback = function()
+      firetouchinterest(Player.HumanoidRootPart, Workspace.Effects.HealthPotion, 0)
+   end,
+})
+
+
+local ToolTab = Window:CreateTab("Tools", nil) -- Title, Image
+
+local ToolParagraph1 = ToolTab:CreateParagraph({Title = "Small Things", Content = "These are probably availiable in other universal scripts but I find it easy to access here"})
+
+local ToolLabel1 = ToolTab:CreateLabel("Walkspeed might break mounts but idk")
+
+local ToolToggle1 = ToolTab:CreateToggle({
+   Name = "Walkspeed Toggle",
+   CurrentValue = false,
+   Flag = "ToolToggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+      WalkspeedToggle = Value
+   end,
+})
+
+local ToolSlider1 = ToolTab:CreateSlider({
+   Name = "Walkspeed",
+   Range = {0, 100},
+   Increment = 1,
+   Suffix = "Studs/Sec",
+   CurrentValue = 16,
+   Flag = "ToolSlider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+      Walkspeed = Value
+   end,
+})
+
+local ToolToggle2 = ToolTab:CreateToggle({
+   Name = "Jump Power Toggle",
+   CurrentValue = false,
+   Flag = "ToolToggle2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+      JumpPowerToggle = Value
+   end,
+})
+
+local ToolSlider2 = ToolTab:CreateSlider({
+   Name = "Jump Power",
+   Range = {0, 200},
+   Increment = 1,
+   Suffix = "Studs",
+   CurrentValue = 50,
+   Flag = "ToolSlider2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+      JumpPower = Value
+   end,
+})
+
+local ToolSection1 = ToolTab:CreateSection("Universal Scripts")
+
+local ToolButton1 = ToolTab:CreateButton({
+   Name = "Infinite Yield",
+   Callback = function()
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))() -- Credit: Infinite Yield
+   end,
+})
+
+local ToolButton2 = ToolTab:CreateButton({
+   Name = "Remote Spy",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastebin.com/raw/9XQ49C5Y'))() -- Credit: SimpleSpy v2.2
+   end,
+})
+
+local ToolButton3 = ToolTab:CreateButton({
+   Name = "Orca",
+   Callback = function()
+      loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua"))() -- Credit: orca
+   end,
+})
+
+local ToolButton4 = ToolTab:CreateButton({
+   Name = "CMD-X",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source",true))() -- Credit: CMD-X
+   end,
+})
+
+local ToolButton5 = ToolTab:CreateButton({
+   Name = "Dex",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/LorekeeperZinnia/Dex/master/main.lua"))() -- Credit: Dark Dex
+   end,
+})
+
+print("Gui Loaded")
+
+
+-- input detector
+UserInputService.InputBegan:Connect(function(input)
+   if AutoQuestToggle then
+      if input.KeyCode == Enum.KeyCode.E or input.KeyCode == Enum.KeyCode.Q then
+         game:GetService("ReplicatedStorage").Remote.AcceptQuest:FireServer("CJ:4")
+      end
+   end
+end)
+
+
+-- health detector
+local function DetectHealth()
+    HealthPercentage = Humanoid.Health / Humanoid.MaxHealth * 100
+end
+
+Humanoid.HealthChanged:Connect(DetectHealth)
