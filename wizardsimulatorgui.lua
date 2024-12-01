b9c7fd39a2f528b5d33fa9964777521d55482ba0
penguin586970 <136866320+P3nguinMinecraft@users.noreply.github.com>
2024-12-01 12:35:27 -0500

    forgot to change name of element

--- wizardsimulatorgui.lua
+++ wizardsimulatorgui.lua
@@ -437,10 +437,10 @@ local QOLToggle4 = QOLTab:CreateToggle({
    end,
 })
 
-local QOLToggle4 = QOLTab:CreateToggle({
+local QOLToggle5 = QOLTab:CreateToggle({
    Name = "Spell No Slow",
    CurrentValue = false,
-   Flag = "QOLToggle4", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Flag = "QOLToggle5", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
       NoSlowToggle = Value
    end,
